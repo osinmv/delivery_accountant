f0701d437e0e473abec38db89d8f1e55c7c4888a
osinmv <osinmv@gmail.com>
2019-08-13 23:13:14 -0400

    added_simple_window

---
+++ frontend.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+
+
+class MainWindow(Frame):
+    def __init__(self,window=None):
+        super().__init__(window)
+        
+        
+        
+    
+
+
+
+root = MainWindow()
+root.master.title("Delivery Accountant")
+
+root.master.minsize(1000,400)
+root.master.resizable(False,False)
+root.mainloop()
+
+
+
+
+
+        
