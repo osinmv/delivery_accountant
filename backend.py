d41251af61ef8550d357acc9c5b6dfb20ae0b785
osinmv <31288523+osinmv@users.noreply.github.com>
2019-08-14 17:02:19 -0400

    started the good way thanks Morice!
    should end vendor list,tasks and customer info! good luck max0

---
+++ backend.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+
+# docket INTEGER,date_client INTEGER,date_require INTEGER, date_shipment INTEGER, requirements BLOB, notes TEXT
--- frontend.py
+++ frontend.py
@@ -8,6 +8,8 @@ HIGHFONT = ("Calibre","18")
 BASICFONT = ("Calibre","14")
 ENTRYFONT = ("Calibre","12")
 
+
+
 data_example = {
     "date":
     {
@@ -52,21 +54,27 @@ class MainWindow(tk.Frame):
         super().__init__(master)
         self.master = master
         self.master.title("Delivery Accountant")
-        self.master.minsize(1280,720)
-        self.master.resizable(False,False)
+        self.master.minsize(1440,1080)
+        self.master.resizable(True,True)
 
 
+        # draws searching engine
+        self.search_engine()
+        # additional info for dates
+        self.draw_dates()
+        self.draw_notes()
+        self.draw_customer_info()
+        #self.general_data()
+        # draws labels and entries with data
+        #self.update_data()
+
+    def search_engine(self):
         #search engine UI
-        self.label_frame_search = tk.LabelFrame(text="Availible Dockets",height="720",width="200")
-        self.label_frame_search.grid(column=0,row=0,sticky="n")
-        self.search_entry = tk.Spinbox(self.label_frame_search,from_=1000,to=100000,bg=BASECOLOR,font=ENTRYFONT)
+        self.label_frame_search = tk.LabelFrame(text="Search",height="720",width="200")
+        self.label_frame_search.place(x = 25,y=10)
+        self.search_entry = tk.Entry(self.label_frame_search,bg=BASECOLOR,font=ENTRYFONT)
         self.search_entry.grid(column=0,row=0)
         self.list_dockets()
-        
-        # additional info for selected docket
-        self.general_data()#draws labels and entries with data
-        self.update_data()
-
 
     #UI labels with dockets
     def list_dockets(self,data=None):
@@ -76,7 +84,7 @@ class MainWindow(tk.Frame):
         if data is None:
             data = []
             for i in range(0,LENGTH_DOCKETS):
-                data.append("Max and Co. - # 30041")
+                data.append("Max and Co. \n # 30041")
 
         #dont forget that there maybe less data than dockets!
         
@@ -85,54 +93,79 @@ class MainWindow(tk.Frame):
             self.docket_labels.append(tk.Label(self.label_frame_search,text=data[i],bg=BASECOLOR,font=BASICFONT).grid(column=0,row=i+1,sticky="w"))   
             #creates the label with data and packs it with grid
     
-    def general_data(self, data=None):
-        """Draws right side of the window with additional info"""
-        if data is None:
-            data = data_example
+    def draw_customer_info(self):
         
-        #ui for the additional data
-        self.label_frame_info = tk.LabelFrame(text="Advanced information",height="720",width="1040")
-        self.label_frame_info.grid(column=1,row=0,sticky="n")
-        self.date_lable = tk.Label(self.label_frame_info,text="Date",font=HIGHFONT,bg=BASECOLOR)
-        self.date_lable.place(x=25,y=25)
+        
+        self.label_frame_customer = tk.LabelFrame(text="Customer Info",height="350",width="300")
+        self.label_frame_customer.place(x=230,y = 10)
+    
+    def draw_vendor_info(self):
+        pass
+    
+    def draw_dates(self):
+        
+        #ui for date widget
+        self.label_frame_dates = tk.LabelFrame(text="About Dates",height="130",width="300")
+        self.label_frame_dates.place(x=1125,y = 10)
         
         #date labels
         # y offset is 40 per new widget
-        x_offset = 40
-        self.date_client = tk.Label(self.label_frame_info,text="client : ",font=BASICFONT,bg=BASECOLOR)
-        self.date_client.place(x=x_offset,y=65)
-        self.date_required = tk.Label(self.label_frame_info,text="required : ",font=BASICFONT,bg=BASECOLOR)
-        self.date_required.place(x=x_offset,y=105)
-        self.date_ship = tk.Label(self.label_frame_info,text="shipment : ",font=BASICFONT,bg=BASECOLOR)
-        self.date_ship.place(x=x_offset,y=145)
+        x_offset = 10
+        self.date_client = tk.Label(self.label_frame_dates,text="Client : ",font=BASICFONT)
+        self.date_client.place(x=x_offset,y=10)
+        self.date_required = tk.Label(self.label_frame_dates,text="Required : ",font=BASICFONT)
+        self.date_required.place(x=x_offset,y=40)
+        self.date_ship = tk.Label(self.label_frame_dates,text="Shipment : ",font=BASICFONT)
+        self.date_ship.place(x=x_offset,y=70)
 
-        x_offset = 150
-        self.entry_client = tk.Entry(self.label_frame_info,font=ENTRYFONT,bg=BASECOLOR)
-        self.entry_client.place(x=x_offset,y = 65)
+        x_offset = 110
+        self.entry_client = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
+        self.entry_client.place(x=x_offset,y = 10)
 
-        self.entry_required = tk.Entry(self.label_frame_info,font=ENTRYFONT,bg=BASECOLOR)
-        self.entry_required.place(x=x_offset,y = 105)
+        self.entry_required = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
+        self.entry_required.place(x=x_offset,y = 40)
 
-        self.entry_ship = tk.Entry(self.label_frame_info,font=ENTRYFONT,bg=BASECOLOR)
-        self.entry_ship.place(x=x_offset,y = 145)
-
-        self.notes_lable = tk.Label(self.label_frame_info,text="Notes",font=HIGHFONT,bg=BASECOLOR)
-        self.notes_lable.place(x=x_offset+350,y=25)
-
-        self.notes = tk.Text(self.label_frame_info,font=ENTRYFONT,width=50,height=25)
-        self.notes.place(x=x_offset+350,y=50)
+        self.entry_ship = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
+        self.entry_ship.place(x=x_offset,y = 70)
+    
+    def draw_tasks(self):
+        pass
+    
+    def draw_notes(self):
         
-    def draw_requirements(self,data=None):
-        """Gets requiremets part of the data"""
+        self.notes_frame = tk.LabelFrame(text="Notes",height="130",width="300")
+        self.notes_frame.place(x=1125,y=600)
+        self.notes = tk.Text(self.notes_frame,font=ENTRYFONT,width=31,height=5)
+        self.notes.place(x=0,y=0)
+
+
+    def general_data(self, data=None):
+        """Draws right side of the window with additional info"""
+        
+        
+        
+
+        
+
+        
+        
+    """def draw_requirements(self,data=None):
+        Gets requiremets part of the data
         
         
         if data == None:
             data = data_example
 
-        
+    
         #requirements
         x_offset = 40
-        y_offset = 200
+        y_offset = 250
+
+        self.reqs_lable = tk.Label(self.label_frame_info,text="Requirements",font=HIGHFONT,bg=BASECOLOR)
+        self.reqs_lable.place(x=x_offset,y=y_offset-40)
+        self.supplier_lable = tk.Label(self.label_frame_info,text="Supplier",font=HIGHFONT,bg=BASECOLOR)
+        self.supplier_lable.place(x=x_offset+250,y=y_offset-40)
+
         self.entries_reqs = []
         #check if all 3 categories are the same length
         for i in range(0,len(data_example["requirements"]["labels"])):
@@ -149,12 +182,12 @@ class MainWindow(tk.Frame):
             entry_supplier.place(x=x_offset + 250  ,y=y_offset)
             self.entries_reqs.append([label_req,check_box,entry_supplier])
             y_offset+=50
-
-
+    """
+    """
     def update_data(self,data=None):
-        """
+        
         parameter data(tuple) receives a named tuple with date info and puts into entries
-        """       
+           
         #date entries(turned off) to change them need to start edit mode
         if data is None:
             data = data_example
@@ -173,10 +206,9 @@ class MainWindow(tk.Frame):
         # filling in requirements
         self.draw_requirements()
         #filing notes
-
-            
-
-        
+        #self.notes.delete(0,tk.END)
+        self.notes.insert(tk.INSERT,data["Notes"])
+    """
     
         
         
@@ -185,9 +217,7 @@ class MainWindow(tk.Frame):
 
 
 root = tk.Tk()
-
 app = MainWindow(master=root)
-
 root.mainloop()
 
 
