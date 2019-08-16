e97645689e456933838c00866a2394e6b825977f
osinmv <31288523+osinmv@users.noreply.github.com>
2019-08-16 11:55:10 -0400

    finished_initial_ui
    have every basic thing. In future add a specific search window. Start Backend probably

--- frontend.py
+++ frontend.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-import logging
+import tkinter.ttk as ttk
+#import logging
 #to start logging the parts of the app
 
 LENGTH_DOCKETS = 11
@@ -44,7 +45,7 @@ data_example = {
     "Notes":"this is for comments, if you have some wishes"
 }
 
-
+tasks_example = [("do this",True,"date"),("do this",True,"date"),("do this",True,"date"),("do this",True,"date")]
 
 
 
@@ -61,11 +62,12 @@ class MainWindow(tk.Frame):
         # draws searching engine
         self.search_engine()
         # additional info for dates
-        self.draw_dates()
-        self.draw_notes()
-        self.draw_customer_info()
-        #self.general_data()
-        # draws labels and entries with data
+        self.draw_dates(535,10)
+        self.draw_notes(535,200)
+        self.customer_info = self.draw_info("Customer",pos_x=230,pos_y=10)
+        self.vendor_info = self.draw_info("Vendor",pos_x=230,pos_y=200)
+        self.draw_tasks(10,pos_x=230,pos_y=390)
+        self.delivery_info(pos_x=725,pos_y=10)
         #self.update_data()
 
     def search_engine(self):
@@ -93,122 +95,160 @@ class MainWindow(tk.Frame):
             self.docket_labels.append(tk.Label(self.label_frame_search,text=data[i],bg=BASECOLOR,font=BASICFONT).grid(column=0,row=i+1,sticky="w"))   
             #creates the label with data and packs it with grid
     
-    def draw_customer_info(self):
+    def delivery_info(self,pos_x,pos_y):
+        """
+        draws delivery information of the form
+        """
+        self.label_frame_delivery = tk.LabelFrame(self.master,text="Delivery Info",height="350",width="300")
+        self.label_frame_delivery.place(x=pos_x,y=pos_y)
+        self.deliver_address = tk.Label(self.label_frame_delivery,text="Delivery Address")
+        self.deliver_address.grid(column=0,row=0)
+        self.entry_del_address = tk.Entry(self.label_frame_delivery,width="25")
+        self.entry_del_address.grid(column=1,row=0)
+
+    def draw_info(self,name,pos_x,pos_y,labels=True):
+        """
+        :parameter: labels: (bool) show the requirement of label of entry
+        :parameter: name: whos info is going to showed up
+        :return: (list) returns two lists that have information about state
+        """
+        label_frame = tk.LabelFrame(self.master,text="{} Info".format(name),height="350",width="300")
+        label_frame.place(x=pos_x,y = pos_y)
+
+        #TODO backend method that returns all customers
+        combobox = ttk.Combobox(label_frame,values=[],font=ENTRYFONT)
+        combobox.grid(column=0,row=0)
+
         
+        name_label = tk.Label(label_frame,text="{} Name".format(name),font=BASICFONT)
+        name_label.grid(column=0,row=1,sticky="w")
         
-        self.label_frame_customer = tk.LabelFrame(text="Customer Info",height="350",width="300")
-        self.label_frame_customer.place(x=230,y = 10)
-    
-    def draw_vendor_info(self):
-        pass
-    
-    def draw_dates(self):
+        address_label = tk.Label(label_frame,text="Address",font=BASICFONT)
+        address_label.grid(column=0,row=2,sticky="w")
+        
+        address_shipping = None
+        
+
+        phone_label = tk.Label(label_frame,text="Phone",font=BASICFONT)
+        phone_label.grid(column=0,row=4,sticky="w")
+
+        contact_label = tk.Label(label_frame,text="Contact",font=BASICFONT)
+        contact_label.grid(column=0,row=5,sticky="w")
+
+        name = None
+        address = None
+        address_ship = None
+        phone = None
+        contact = None
+        #if need lbels than it is going to draw labels, if not draw entries
+        if labels:
+
+            name = tk.Label(label_frame,text="Name")
+            name.grid(column=1,row=1)
+
+            address = tk.Label(label_frame,text="o")
+            address.grid(column=1,row=2)
+        
+
+
+            phone = tk.Label(label_frame,text="88005553535")
+            phone.grid(column=1,row=4)
+
+            contact = tk.Label(label_frame,text="Some Name")
+            contact.grid(column=1,row=5)
+        else:
+            name = tk.Entry(label_frame)
+            name.grid(column=1,row=1)
+
+            address = tk.Entry(label_frame)
+            address.grid(column=1,row=2)
+
+            if name=="Customer":
+                address_ship = tk.Entry(label_frame)
+                address_ship.grid(column=1,row=3)
+                print("i should be there")
+
+            phone = tk.Entry(label_frame)
+            phone.grid(column=1,row=4)
+
+            contact = tk.Entry(label_frame)
+            contact.grid(column=1,row=5)
+
+        
+        return [[name_label,address_label,phone_label,contact_label],[combobox,name,address,phone,contact]]
+
+
+
+
+   
+    def draw_dates(self,pos_x,pos_y):
         
         #ui for date widget
         self.label_frame_dates = tk.LabelFrame(text="About Dates",height="130",width="300")
-        self.label_frame_dates.place(x=1125,y = 10)
+        self.label_frame_dates.place(x=pos_x,y = pos_y)
         
         #date labels
-        # y offset is 40 per new widget
-        x_offset = 10
-        self.date_client = tk.Label(self.label_frame_dates,text="Client : ",font=BASICFONT)
-        self.date_client.place(x=x_offset,y=10)
-        self.date_required = tk.Label(self.label_frame_dates,text="Required : ",font=BASICFONT)
-        self.date_required.place(x=x_offset,y=40)
-        self.date_ship = tk.Label(self.label_frame_dates,text="Shipment : ",font=BASICFONT)
-        self.date_ship.place(x=x_offset,y=70)
+        
+        self.date_client = tk.Label(self.label_frame_dates,text="Client",font=BASICFONT)
+        self.date_client.grid(column = 0,row = 0,sticky="w")
+        self.date_required = tk.Label(self.label_frame_dates,text="Required",font=BASICFONT)
+        self.date_required.grid(column = 0,row = 1,sticky="w")
+        self.date_ship = tk.Label(self.label_frame_dates,text="Shipment",font=BASICFONT)
+        self.date_ship.grid(column = 0,row = 2,sticky="w")
 
-        x_offset = 110
-        self.entry_client = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
-        self.entry_client.place(x=x_offset,y = 10)
+        self.entry_client = tk.Label(self.label_frame_dates,text="2019/03/03",font=ENTRYFONT)
+        self.entry_client.grid(column = 1,row = 0,sticky="w")
 
-        self.entry_required = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
-        self.entry_required.place(x=x_offset,y = 40)
+        self.entry_required = tk.Label(self.label_frame_dates,text="2019/03/03",font=ENTRYFONT)
+        self.entry_required.grid(column = 1,row = 1,sticky="w")
 
-        self.entry_ship = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
-        self.entry_ship.place(x=x_offset,y = 70)
+        self.entry_ship = tk.Label(self.label_frame_dates,text="2019/03/03",font=ENTRYFONT)
+        self.entry_ship.grid(column = 1,row = 2,sticky="w")
+
+
+        #self.entry_client = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
+        #self.entry_client.grid(column = 1,row = 0,sticky="w")
+
+        #self.entry_required = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
+        #self.entry_required.grid(column = 1,row = 1,sticky="w")
+
+        #self.entry_ship = tk.Entry(self.label_frame_dates,font=ENTRYFONT)
+        #self.entry_ship.grid(column = 1,row = 2,sticky="w")
     
-    def draw_tasks(self):
-        pass
     
-    def draw_notes(self):
+    def draw_tasks(self,length,pos_x,pos_y):
+        """
+        draws tasks in this format Task: goal(string)  |  checkbox(bool)  |  target date(date)
+        :parameter: length: (integer) tells how any tasks to draw 
+        
+        """
+        self.tasks = []
+        self.label_frame_tasks = tk.LabelFrame(text="Tasks",height="130",width="300")
+        self.label_frame_tasks.place(x=pos_x, y=pos_y)
+
+        for i in range(0,length):
+            entry = tk.Entry(self.label_frame_tasks,width="75")
+            entry.grid(column=0,row=i)
+
+            check_button = tk.Checkbutton(self.label_frame_tasks)
+            check_button.grid(column=1,row=i)
+
+            date = tk.Entry(self.label_frame_tasks,width="20")
+            date.grid(column=2,row=i)
+            self.tasks.append([entry,check_button,date])
+
+
+
+    
+    
+    def draw_notes(self,pos_x,pos_y):
         
         self.notes_frame = tk.LabelFrame(text="Notes",height="130",width="300")
-        self.notes_frame.place(x=1125,y=600)
+        self.notes_frame.place(x=pos_x,y=pos_y)
         self.notes = tk.Text(self.notes_frame,font=ENTRYFONT,width=31,height=5)
-        self.notes.place(x=0,y=0)
+        self.notes.grid(column=0,row=0)
 
 
-    def general_data(self, data=None):
-        """Draws right side of the window with additional info"""
-        
-        
-        
-
-        
-
-        
-        
-    """def draw_requirements(self,data=None):
-        Gets requiremets part of the data
-        
-        
-        if data == None:
-            data = data_example
-
-    
-        #requirements
-        x_offset = 40
-        y_offset = 250
-
-        self.reqs_lable = tk.Label(self.label_frame_info,text="Requirements",font=HIGHFONT,bg=BASECOLOR)
-        self.reqs_lable.place(x=x_offset,y=y_offset-40)
-        self.supplier_lable = tk.Label(self.label_frame_info,text="Supplier",font=HIGHFONT,bg=BASECOLOR)
-        self.supplier_lable.place(x=x_offset+250,y=y_offset-40)
-
-        self.entries_reqs = []
-        #check if all 3 categories are the same length
-        for i in range(0,len(data_example["requirements"]["labels"])):
-            label_req = tk.Label(self.label_frame_info,text=data["requirements"]["labels"][i],font=BASICFONT,bg=BASECOLOR)
-            label_req.place(x=x_offset,y=y_offset)
-            check_box = tk.Checkbutton(self.label_frame_info)
-            check_box.place(x=x_offset + 150 ,y=y_offset)
-            if data["requirements"]["checklist"][i]:
-                check_box.select()
-            check_box.configure(state="disabled")
-            entry_supplier = tk.Entry(self.label_frame_info,font=ENTRYFONT,bg=BASECOLOR)
-            entry_supplier.insert(0,data["requirements"]["supplier"][i])
-            entry_supplier.configure(state="readonly")
-            entry_supplier.place(x=x_offset + 250  ,y=y_offset)
-            self.entries_reqs.append([label_req,check_box,entry_supplier])
-            y_offset+=50
-    """
-    """
-    def update_data(self,data=None):
-        
-        parameter data(tuple) receives a named tuple with date info and puts into entries
-           
-        #date entries(turned off) to change them need to start edit mode
-        if data is None:
-            data = data_example
-        
-        #filling in dates
-        self.entry_client.delete(0,tk.END)
-        self.entry_client.insert(0,data["date"]["client"])
-        self.entry_client.configure(state="readonly")
-        self.entry_required.delete(0,tk.END)
-        self.entry_required.insert(0,data["date"]["required"])
-        self.entry_required.configure(state="readonly")
-        self.entry_ship.delete(0,tk.END)
-        self.entry_ship.insert(0,data["date"]["ship"])
-        self.entry_ship.configure(state="readonly")
-
-        # filling in requirements
-        self.draw_requirements()
-        #filing notes
-        #self.notes.delete(0,tk.END)
-        self.notes.insert(tk.INSERT,data["Notes"])
-    """
     
         
         
